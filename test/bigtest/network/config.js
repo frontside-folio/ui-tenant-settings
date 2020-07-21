0e0e6de1a3cc024b1c41291fce430fba8a27594f
minkimcello@gmail.com <minkimcello@gmail.com>
2020-07-21 08:34:24 -0400

    Add comment explaining inflections

--- test/bigtest/network/config.js
+++ test/bigtest/network/config.js
@@ -1,6 +1,7 @@
 import { inflections } from 'inflected';
 
 export default function config() {
+  // This is to add exceptions to models/factories that can't be pluralized because they end with 's'
   inflections('en', function (inflect) {
     inflect.irregular('campus', 'campus');
   });
